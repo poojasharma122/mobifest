dadd4f542fb75ab2702d02333de44df73ca2209b
frontenddev <frontendev344@gmail.com>
2025-05-05 21:17:29 +0530

    fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,26 @@
 });
 
   // Read More Functionality JS end
+
+
+
+  // mobil
+document.addEventListener("DOMContentLoaded", function () {
+  const searchIcon = document.getElementById("search-icon");
+  const nav = document.querySelector(".nav");
+  const menuToggle = document.querySelector(".menu-toggle");
+
+  if (searchIcon) {
+      searchIcon.addEventListener("click", function () {
+          nav.classList.toggle("search");
+          nav.classList.toggle("no-search");
+      });
+  }
+
+  if (menuToggle) {
+      menuToggle.addEventListener("click", function () {
+          nav.classList.toggle("mobile-nav");
+          this.classList.toggle("is-active");
+      });
+  }
+});
